4eeed6dc9204abe51f08e79ad4a20f5d41e103f5
asep-sang-raja-iblis <saefuddinasep@yahoo.com>
2024-06-21 00:43:49 +0700

    menambahkan detail

---
+++ src/navigations/HomeStackNavigation.tsx
@@ -0,0 +1,42 @@
+// // navigations/HomeStackNavigation.tsx
+// import React from 'react';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import Home from '../screens/Home';
+// import MovieDetail from '../screens/MovieDetail';
+
+// const Stack = createStackNavigator();
+
+// const HomeStackNavigation = (): JSX.Element => (
+//   <Stack.Navigator>
+//     <Stack.Screen
+//       name="Home"
+//       component={Home}
+//       options={{ headerShown: false }}
+//     />
+//     <Stack.Screen
+//       name="MovieDetail"
+//       component={MovieDetail}
+//       options={{ headerShown: false }}
+//     />
+//   </Stack.Navigator>
+// );
+
+// export default HomeStackNavigation;
+
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from '../screens/Home';
+import MovieDetail from '../screens/MovieDetail';
+
+const Stack = createNativeStackNavigator();
+
+const HomeStackNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="MovieDetail" component={MovieDetail} />
+    </Stack.Navigator>
+  );
+};
+
+export default HomeStackNavigation;
